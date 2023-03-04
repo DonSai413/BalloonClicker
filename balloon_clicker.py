f425bc1eeba881ff53ecf8d03dd5bae95822c42b
DonSai413 <76758588+DonSai413@users.noreply.github.com>
2023-03-04 16:08:26 -0600

    Circles stay, but points are broken

--- balloon_clicker.py
+++ balloon_clicker.py
@@ -21,16 +21,7 @@ class Square(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
 #Test End
 
-class Circle(pygame.sprite.Sprite):
-	radius = 20
-	balloonX = random.randrange(1, 799)
-	balloonY = random.randrange(1, 599)
-	red = random.randrange(100, 150)
-	green = random.randrange(100, 150)
-	blue = random.randrange(100, 150)
-	color = (red, green, blue)
-	thickness = 0;
-	center_point = (balloonX, balloonY)
+
 
 image = pygame.image.load("sky_image.jpg")
 
@@ -41,6 +32,34 @@ pygame.display.set_caption("Balloon Clicker")
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
+class Circle(pygame.sprite.Sprite):
+	index = 0
+	radius = 20
+	def __new__(cls, *args, **kwargs):
+		print("1. Create a new instance of Circle.")
+		return super().__new__(cls)
+	def __init__(self):
+		self.balloonX = random.randrange(1, 799)
+		self.balloonY = random.randrange(1, 599)
+		self.red = random.randrange(100, 150)
+		self.green = random.randrange(100, 150)
+		self.blue = random.randrange(100, 150)
+		self.color = (self.red, self.green, self.blue)
+		self.thickness = 0;
+		self.center_point = (self.balloonX, self.balloonY)
+	#random.seed(clock.get_time())
+	"""balloonX = random.randrange(1, 799)
+	balloonY = random.randrange(1, 599)
+	red = random.randrange(100, 150)
+	green = random.randrange(100, 150)
+	blue = random.randrange(100, 150)
+	color = (red, green, blue)
+	thickness = 0;
+	center_point = (balloonX, balloonY)"""
+	def drawCircle(self):
+		#print("DrawCircle is running.")
+		#print(self.balloonX,self.balloonY)
+		pygame.draw.circle(screen, self.color, self.center_point, self.radius, self.thickness)
 #SPRITE INITIALIZATION
 #Test Start
 square1 = Square()
@@ -53,9 +72,12 @@ square4 = Square()
 #Test End
 
 #VARIABLES
-	#Balloon Position 
-balloonX = 100
-balloonY = 100
+	#Balloon Default Values 
+#balloonX = 100
+#balloonY = 100
+radius = 20
+thickness = 0
+balloonSpawn = 1
 
 font1 = pygame.font.SysFont('freesanbold.ttf', 50)
 mouse1 = False
@@ -63,8 +85,12 @@ mouse1 = False
 xchange = 10
 ychange = 10
 
-points = 0
+circleCounter = 0;
 
+points = 0
+pointsValue = 1
+
+circleGroup = list()
 play = True
 
 zeroZero = (0,0)
@@ -75,8 +101,9 @@ pointsBox = text1.get_rect()
 
 pointsBox.center = (700, 100)
 
-clockSpeed = .5
+clockSpeed = 1
 #GAME LOOP
+#circleGroup.append(Circle())
 while play:
 	clock.tick(clockSpeed)
 	#ticks = pygame.time.get_ticks()
@@ -86,15 +113,18 @@ while play:
 		if event.type == KEYDOWN:
 			if event.key == K_BACKSPACE:
 				play = False
-		elif event.type == QUIT:
-			play = False
 		elif event.type == MOUSEBUTTONDOWN:
 			mouse1 = True
 			mousePos = pygame.mouse.get_pos()
-			if mousePos >= (balloonX - radius, balloonY - radius) and mousePos <= (balloonX + radius, balloonY + radius):
-				points += 1
+			for circle in circleGroup:
+				if mousePos >= (circle.balloonX - circle.radius, circle.balloonY - circle.radius) and mousePos <= (circle.balloonX + circle.radius, circle.balloonY + circle.radius):
+					points += pointsValue
+					newint = circle.index
+					del circleGroup[newint]
 		elif event.type == MOUSEBUTTONUP:
 			mouse1 = False
+		elif event.type == QUIT:
+			play = False
 	#Test Start
 	screen.fill((0, 0, 0))
 	#points += 1
@@ -110,17 +140,23 @@ while play:
 	screen.blit(square2.surf,(40,530))
 	screen.blit(square3.surf,(730,40))
 	screen.blit(square4.surf,(730,530))
-	radius = 20
 	text1 = font1.render(f"Points: {points}", True, (0, 255, 0))
-	balloonX = random.randrange(1, 799)
-	balloonY = random.randrange(1, 599)
-	red = random.randrange(100, 150)
-	green = random.randrange(100, 150)
-	blue = random.randrange(100, 150)
-	color = (red, green, blue)
-	thickness = 0;
-	center_point = (balloonX, balloonY)
+	#circleGroup.append(Circle())
+	for x in range(balloonSpawn):
+		circleGroup.append(Circle())
+		for circle in circleGroup:
+			circle.index = circleCounter
+			circle.drawCircle()
+			circleCounter += 1
+		"""balloonX = random.randrange(1, 799)
+		balloonY = random.randrange(1, 599)
+		red = random.randrange(100, 150)
+		green = random.randrange(100, 150)
+		blue = random.randrange(100, 150)
+		color = (red, green, blue)
+		center_point = (balloonX, balloonY)
+		pygame.draw.circle(screen, color, center_point, radius, thickness)"""
 	screen.blit(text1, pointsBox)
-	pygame.draw.circle(screen, color, center_point, radius, thickness)
+	circleCounter = 0
 	#Test End
 	pygame.display.flip()
